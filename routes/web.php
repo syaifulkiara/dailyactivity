427fd658f6a3ddcf933ea88b7bc862edfe230b3f
SyaifulKiara <58905713+syaifulkiara@users.noreply.github.com>
2021-07-31 00:37:02 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use App\Http\Controllers\ActivityController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/print', function () {
+    return view('activity.print');
+});
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
@@ -21,6 +23,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/monitoring', [App\Http\Controllers\MonitoringController::class, 'index'])->name('monitoring');
 Route::resource('activity', ActivityController::class);
+Route::get('/print', [App\Http\Controllers\PrintController::class, 'index'])->name('print');
 Route::get('profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
 Route::get('/profile/{id}/edit', [App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
 Route::post('/profile/{id}/update', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
